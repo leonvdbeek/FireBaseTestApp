1f6ed44a56350d3b81ca4650ea8f4061a060ff19
Léon van de Beek <leon.vandebeek@xs4all.nl>
2020-11-14 13:46:53 +0100

    Fixed messages and div

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function SignOut() {
 function ChatRoom() {
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt', 'asc').limitToLast(25);
 
   const [messages] = useCollectionData(query, { idField: 'id' });
 
